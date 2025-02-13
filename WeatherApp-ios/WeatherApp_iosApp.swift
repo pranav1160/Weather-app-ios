5a97d80f6cdd9af1c28144c1ca8b120eca90d61a
Pranav <pranav1160ly@gmail.com>
2025-02-13 18:14:22 +0530

    Initial Commit

---
+++ WeatherApp-ios/WeatherApp_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_iosApp.swift
+//  WeatherApp-ios
+//
+//  Created by Pranav on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
